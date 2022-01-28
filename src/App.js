42ba6d249a5ce86d5a05cad1f8531c0332c1244a
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-01-28 16:10:20 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Subs } from "./Subs";
 
 function App() {
   return (
     <div className="App">
-
+      <div className="outerContainer">
+        <Subs
+          cardType="F R E E"
+          user={"Single User"}
+          storage="5GB Storage"
+          private={false}
+          phone={false}
+          domain={false}
+          report={false}
+        />
+        <Subs
+          cardType="F R E E"
+          user={"Single User"}
+          storage="5GB Storage"
+          private={false}
+          phone={false}
+          domain={false}
+          report={false}
+        />
+        <Subs
+          cardType="P R O"
+          user="Unlimited Users"
+          storage="150GB Storage"
+          private={true}
+          phone={true}
+          domain="unlimited"
+          report={true}
+        />
+        <Subs
+          cardType="P L U S"
+          user="5 Users"
+          storage="50GB Storage"
+          private={true}
+          phone={true}
+          domain={true}
+          report={false}
+        />
+      </div>
     </div>
   );
 }
